00adf32f1ce29924257ddf87e9b35590742181f4
Drake <mdrakea3@tutanota.com>
2021-01-15 12:47:47 -0500

    Add website

---
+++ docs/app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, redirect, url_for,request
+from flask import make_response
+import subprocess
+app = Flask(__name__)
+result = 'ERROR: GENERATION FAILED. PLEASE REFRESH'
+@app.route("/")
+def index():
+    return render_template('input.html')
+
+@app.route('/', methods=['POST'])
+def index_post():
+    word = request.form['word']
+    subprocess.call('./produce.py ' + word, shell=True)
+    with open('result.txt') as f:
+        result = ''
+        for line in f:
+            result = result + '\n' + line
+    return result
+    
+#@app.route("/result", methods=['POST'])
+#def result():
+#    if request.method == 'POST':
+#        text = request.form['text']
+#        processed_text = text.upper()
+#        return processed_text
