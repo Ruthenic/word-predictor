1f475c49c2a5f5afffbebd69f09ebc6c85d1caf9
Drake <mdrakea3@tutanota.com>
2021-01-15 13:16:20 -0500

    Make produce.py used for web produce-web.py

---
+++ docs/produce-web.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+import sys,random
+letter = sys.argv[1]
+howLong = 100
+oldLetter = ''
+full = []
+with open('result.txt', 'w') as f:
+    f.write(letter + ' ')
+letter= letter[len(letter) -1]
+for temp in range(howLong):
+    weights=[]
+    possibles = []
+    amount = 0
+    with open('counts.txt') as f:
+        for line in f:
+            if line.startswith(letter):
+                possibles.append(line.replace('\n', '').replace('\\n', ''))
+    print(possibles)
+    #for i in possibles:
+    #    try:
+    #        if int(i.split(';')[2]) > amount and not i == oldLetter and random.randrange(0,10) > 2:
+    #            amount = int(i.split(';')[2])
+    #            oldLetter = i
+    #            nextletter = i.split(';')[1]
+    #    except:
+    #        pass
+    for i in possibles:
+        weights.append(int(i.split(';')[2]))
+    try:
+        nextletter = random.choices(possibles, weights=weights, k=1)[0].split(';')[1]
+    except:
+        nextletter = random.choice(full)
+        pass
+    print(nextletter)
+    with open('result.txt', 'a') as f:
+        f.write(nextletter + ' ')
+    full.append(nextletter)
+    letter = nextletter
