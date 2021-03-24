450deee13ece4ba8e6d4da511130b054e6941c76
Ruthenic <mdrakea3@tutanota.com>
2021-03-24 16:17:02 -0400

    add new wasm code

--- rust-version/produce-wp/src/lib.rs
+++ rust-version/produce-wp/src/lib.rs
@@ -4,29 +4,12 @@ use rand::prelude::*;
 use std::fs::File;
 use std::io;
 use std::io::prelude::*;
-
-fn main() {
-    println!("How many words would you like to make? ");
-    let mut line = String::new();
-    std::io::stdin().read_line(&mut line).ok().unwrap();
-    line.pop();
-    //println!("{}", line);
-    let mut num_of_chars = line.parse::<i32>().ok().unwrap();
-    num_of_chars = num_of_chars - 1;
-    println!("What word would you like to start with? ");
-    let mut line = String::new();
-    std::io::stdin().read_line(&mut line);
-    line.pop();
-    let mut prev_char = line;
+pub fn gen(line: String, num_of_chars: i32, values: String) {
     //println!("{}", prev_char);
+    let mut prev_char = line;
     let mut index = 0;
     let mut choices: Vec<String> = vec![];
     let mut weights: Vec<i32> = vec![];
-    let mut f = File::open("counts.txt").ok().unwrap();
-    //let mut f = File::open("counts.txt").expect("unable to find counts.txt");
-    let mut file_string = String::new();
-    f.read_to_string(&mut file_string);
-    let values = file_string;
     let mut file = std::fs::File::create("out.txt").expect("create failed");
     loop {
         println!("{}", prev_char);
